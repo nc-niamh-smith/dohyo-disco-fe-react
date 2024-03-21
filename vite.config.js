82eca5ec04ebd819228d9e8027960498976a4ba4
nc-niamh-smith <niamh.smith@northcoders.com>
2024-03-21 10:14:46 +0000

    general set up for testing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    setupFiles: './tests/setup.js',
+    environment: 'jsdom',
+    include: ['jsx', '**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**', '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*']
+  }
+}
+)
